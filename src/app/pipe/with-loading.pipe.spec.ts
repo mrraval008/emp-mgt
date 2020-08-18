b8ccc5bd184814ead82990f0bc0b981538a92008
Milan Raval <ravalm@Riversand.com>
2020-08-18 19:04:47 +0530

    Auto-generated commit

--- src/app/pipe/with-loading.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WithLoadingPipe } from './with-loading.pipe';
-
-describe('WithLoadingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new WithLoadingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
